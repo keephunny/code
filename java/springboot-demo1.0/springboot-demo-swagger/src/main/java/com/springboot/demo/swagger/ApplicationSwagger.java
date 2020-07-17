55941e60bacf30138641e516364e9c94fecd8f29
keephunny <keephunny@163.com>
2020-07-17 17:52:57 +0800

    d

---
+++ java/springboot-demo1.0/springboot-demo-swagger/src/main/java/com/springboot/demo/swagger/ApplicationSwagger.java
@@ -0,0 +1,13 @@
+package com.springboot.demo.swagger;
+
+/**
+ * Hello world!
+ *
+ */
+public class ApplicationSwagger
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
