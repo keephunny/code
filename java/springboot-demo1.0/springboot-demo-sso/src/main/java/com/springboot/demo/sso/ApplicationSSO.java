c30d49a062ea710c221dda24208cf144d99f6c39
keephunny <keephunny@163.com>
2020-09-17 10:02:27 +0800

    d

---
+++ java/springboot-demo1.0/springboot-demo-sso/src/main/java/com/springboot/demo/sso/ApplicationSSO.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020.
+ */
+package com.springboot.demo.sso;
+
+/**
+ * //TODO 添加说明
+ *
+ * @author 汪强
+ * 创建时间 2020-09-14 15:53
+ */
+public class ApplicationSSO {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
